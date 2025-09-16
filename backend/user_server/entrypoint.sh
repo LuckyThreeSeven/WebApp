566a73359e34b629f6a3ccd571d4926067d0027f
uridonget <100407513+uridonget@users.noreply.github.com>
2025-09-16 20:22:04 +0900

    user db table added (#8)

--- backend/user_server/entrypoint.sh
+++ backend/user_server/entrypoint.sh
@@ -5,19 +5,18 @@ while ! nc -z "$DB_HOST" "$DB_PORT"; do
     sleep 3
 done
 
-python manage.py makemigrations account authentication follow log
+python manage.py makemigrations users
 python manage.py migrate
 python manage.py collectstatic --noinput
 
 python manage.py shell -c "
 from django.contrib.auth import get_user_model;
 User = get_user_model();
-username = '$POSTGRES_SUPER_USER';
 email = '$POSTGRES_SUPER_EMAIL';
 password = '$POSTGRES_SUPER_PASSWORD';
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password);
-    print(f'Superuser \"{username}\" created.');
+if not User.objects.filter(email=email).exists():
+    User.objects.create_superuser(email=email, password=password);
+    print(f'Superuser with email \"{email}\" created.');
 "
 
 exec "$@"
