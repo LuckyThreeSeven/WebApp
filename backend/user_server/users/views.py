f254d26b845b4ed962e2be0960dd665a6876bf8f
uridonget <100407513+uridonget@users.noreply.github.com>
2025-09-25 16:05:33 +0900

    Apply black formatter

--- backend/user_server/users/views.py
+++ backend/user_server/users/views.py
@@ -66,7 +66,7 @@ def signup(request):
     # Call mail-server to send the email
     try:
         # This is an assumed endpoint for the mail-server.
-        mail_server_url = os.getenv("MAIL_API_URL","http://mail-server:8000/api/email")
+        mail_server_url = os.getenv("MAIL_API_URL", "http://mail-server:8000/api/email")
         response = requests.post(
             mail_server_url,
             json={
