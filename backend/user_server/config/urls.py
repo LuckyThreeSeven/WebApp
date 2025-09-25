5e98b4f5bb18745540a7c79a13aa107462c1abed
uridonget <100407513+uridonget@users.noreply.github.com>
2025-09-25 15:50:24 +0900

    Delete Admin Page Settings

--- backend/user_server/config/urls.py
+++ backend/user_server/config/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -26,7 +25,6 @@ from drf_spectacular.views import (
 )
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
