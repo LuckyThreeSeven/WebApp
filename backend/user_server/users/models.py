905636b11c21c742be5ffbf3bcaf790aeced0d89
uridonget <100407513+uridonget@users.noreply.github.com>
2025-09-26 10:39:09 +0900

    Delete Verification_code, expiry_time and is_active from User table

--- backend/user_server/users/models.py
+++ backend/user_server/users/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 from django.db import models
+from django.utils import timezone
 
 
 class UserManager(BaseUserManager):
@@ -17,26 +18,11 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-
-        return self.create_user(email, password, **extra_fields)
-
 
 class User(AbstractBaseUser, PermissionsMixin):
     uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True, db_index=True)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
-    verification_code = models.CharField(max_length=5, blank=True, null=True)
-    code_expiry = models.DateTimeField(blank=True, null=True)
-
+    created_at = models.DateTimeField(default=timezone.now)
     objects = UserManager()
 
     USERNAME_FIELD = "email"
