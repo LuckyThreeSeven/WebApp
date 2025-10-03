6998482ddd2c723c267e91dc202279735bf3eea4
“kwl46” <yjsco653@naver.com>
2025-10-03 19:26:46 +0900

    Add prometheus for Django

--- backend/user_server/config/settings.py
+++ backend/user_server/config/settings.py
@@ -41,6 +41,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_prometheus.middleware.PrometheusAfterMiddleware",  # for prometheus
 ]
 ROOT_URLCONF = "config.urls"
 TEMPLATES = [
