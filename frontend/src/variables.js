6099e4389f3239787530eeca5d9212cc1057115a
uridonget <100407513+uridonget@users.noreply.github.com>
2025-10-15 14:50:38 +0900

    FrontEnd URL fixed

--- frontend/src/variables.js
+++ frontend/src/variables.js
@@ -1,3 +1,3 @@
-export const USER_SERVER_URL = 'https://api.neves-box/users';
-export const STATUS_SERVER_URL = 'https://api.neves-box/status';
-export const PLAY_SERVER_URL = 'https://api.neves-box/videos';
+export const USER_SERVER_URL = 'https://api.neves-box.com/users';
+export const STATUS_SERVER_URL = 'https://api.neves-box.com/status';
+export const PLAY_SERVER_URL = 'https://api.neves-box.com/videos';
